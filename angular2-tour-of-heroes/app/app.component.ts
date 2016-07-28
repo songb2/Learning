85c9953e62e039f39d11b2920499b07077c97228
bruce song <BruceSong2010@hotmail.com>
2016-07-28 16:17:06 +0800

    create a reusable service to manage our hero data calls

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,19 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
 
 @Component({
     selector: 'my-app',
@@ -79,12 +69,23 @@ const HEROES: Hero[] = [
             border-radius: 4px 0 0 4px;
           }
         `],
-    directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit { 
     title = 'Tour of Heroes';
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
+
+    constructor(private heroService: HeroService) { }
+
+    getHeros() {
+        this.heroService.getHerosSlowly().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit() {
+        this.getHeros();
+    }   
 
     onSelect(hero: Hero) { this.selectedHero = hero; }
 }
