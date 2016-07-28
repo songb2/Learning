61147f6e4c9d6ecb4e1685792e3b79eebb9a2440
bruce song <BruceSong2010@hotmail.com>
2016-07-28 11:09:28 +0800

    5 min quickstart of angular2 with typescript

---
+++ Angular2QuickStart/app/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello World, this is my first Angular 2 App via TypeScript.</h1>'
+})
+export class AppComponent { }
